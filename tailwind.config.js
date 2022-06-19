84bf8157ebfc965876e14116de8e6f61a00ad645
developedBySJ <developedbysj@gmail.com>
2022-06-19 21:51:30 +0530

    Setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        green: colors.emerald,
+        yellow: colors.amber,
+        purple: colors.violet,
+        primary: {
+          100: "#def7ec",
+          200: "#bcf0da",
+          300: "#84e1bc",
+          400: "#31c48d",
+          500: "#0e9f6e",
+          600: "#057a55",
+          700: "#046c4e",
+          800: "#03543f",
+          900: "#014737",
+        },
+        gray: {
+          100: "#FBFAFC",
+          200: "#F7F5FA",
+          300: "#F1EDF7",
+          400: "#DFDAE8",
+          500: "#BFB8CC",
+          600: "#9187A1",
+          700: "#7D728F",
+          800: "#6A5F7A",
+          900: "#453C52",
+        },
+      }
+    },
+  },
+  plugins: [],
+}
