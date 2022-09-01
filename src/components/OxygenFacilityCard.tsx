93751b95c8cc2cd27711086bc9255614b49a3555
developedBySJ <developedbysj@gmail.com>
2022-09-01 16:26:10 +0530

    Add facility link to card

--- src/components/OxygenFacilityCard.tsx
+++ src/components/OxygenFacilityCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'raviger'
 import React from 'react'
 import { Phone } from 'react-feather'
 import {
@@ -15,17 +16,27 @@ export const OxygenFacilityCard: React.FC<OxygenFacilityCardProps> = ({
   className,
   data,
 }) => {
-  const { facility_name, facility_last_updated, facility_type, phone_number } =
-    data
+  const {
+    facility_name,
+    facility_last_updated,
+    facility_type,
+    phone_number,
+    facility_id,
+  } = data
   const tableData = getOxygenTableRows(data)
   return (
     <div className={className}>
       <div className="flex justify-between gap-2 flex-wrap">
         <div className="flex items-center gap-4">
           <h1 className="dark:text-white text-3xl font-medium capitalize">
-            {facility_name}
+            <Link
+              href={`/facility/${facility_id || ''}`}
+              className="text-lg font-medium capitalize"
+            >
+              {facility_name}
+            </Link>
           </h1>
-          <span className="font-medium text-sm border-blue-700 dark:bg-blue-900 bg-blue-200 rounded-full py-1 px-2 text-black dark:text-white">
+          <span className="bg-primary-300 text-primary-900 font-bold text-sm px-2 rounded">
             {facility_type}
           </span>
         </div>
