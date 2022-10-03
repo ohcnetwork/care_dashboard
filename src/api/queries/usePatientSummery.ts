f3e27a014a80b4a5b43123d87c70f0e8e3edb140
Khavin Shankar <khavinshankar@gmail.com>
2022-10-02 21:16:55 -0700

    Added logic for burn rate (#23)
    * fix (FacilityDetails): removed high alerts
    
    * feat (FacilityDetails): added logic for burn rate
    
    * refactor: removed console logs
    
    * refactor (FacilityDetails): extracted expected burn rate availablity check into a variable

--- src/api/queries/usePatientSummery.ts
+++ src/api/queries/usePatientSummery.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useQuery } from 'react-query'
 import { PaginatedResponse } from '../../types/paginatedResponse'
 import { createQueryKey } from '../../utils/url'
-import { FacilitySummaryResponse } from './useFacilitySummary'
+import { Facility } from './useFacilitySummary'
 
 const PATIENT_SUMMARY_KEY = 'patientSummeryKey'
 
@@ -15,12 +15,42 @@ export interface PatientSummaryQuery {
   facility?: string
 }
 
+export interface PatientSummaryResponse {
+  facility: Facility
+  created_date: string
+  modified_date: string
+  data: Data
+}
+
+interface Data {
+  district: string
+  facility_external_id: string
+  facility_name: string
+  modified_date: string
+  today_patients_bed_with_oxygen_support: number
+  today_patients_home_quarantine: number
+  today_patients_icu: number
+  today_patients_icu_with_invasive_ventilator: number
+  today_patients_icu_with_non_invasive_ventilator: number
+  today_patients_icu_with_oxygen_support: number
+  today_patients_isolation: number
+  today_patients_regular: number
+  total_patients_bed_with_oxygen_support: number
+  total_patients_home_quarantine: number
+  total_patients_icu: number
+  total_patients_icu_with_invasive_ventilator: number
+  total_patients_icu_with_non_invasive_ventilator: number
+  total_patients_icu_with_oxygen_support: number
+  total_patients_isolation: number
+  total_patients_regular: number
+}
+
 export const usePatientSummary = (query: PatientSummaryQuery, enabled = true) =>
   useQuery(
     createQueryKey(PATIENT_SUMMARY_KEY, query),
     () =>
       axios
-        .get<PaginatedResponse<FacilitySummaryResponse[]>>(
+        .get<PaginatedResponse<PatientSummaryResponse[]>>(
           'https://careapi.coronasafe.in/api/v1/patient_summary/',
           {
             params: query,
