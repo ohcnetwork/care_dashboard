3e48149c94bafd496905243f545d1ef69e46730b
Khavin Shankar <khavinshankar@gmail.com>
2022-10-10 09:36:43 +0530

    fix: reduced the fuzziness of search results

--- src/utils/hooks/usePaginateData.ts
+++ src/utils/hooks/usePaginateData.ts
@@ -13,13 +13,13 @@ const DEFAULT_RESULTS_PER_PAGE = 10
 export const usePaginateData = <T>(args: UsePaginationDataArgs<T>) => {
   const { data, keys, searchValue, resultsPerPage } = args
 
-  const fuseData = useRef(new Fuse(data, { keys }))
+  const fuseData = useRef(new Fuse(data, { keys, includeScore: true }))
 
   const [page, setPage] = useState(1)
   const [paginatedData, setPaginatedData] = useState(data)
 
   useEffect(() => {
-    fuseData.current = new Fuse(data, { keys })
+    fuseData.current = new Fuse(data, { keys, includeScore: true })
   }, [data])
 
   useEffect(() => {
@@ -27,8 +27,12 @@ export const usePaginateData = <T>(args: UsePaginationDataArgs<T>) => {
     const end = skip + (resultsPerPage || DEFAULT_RESULTS_PER_PAGE)
 
     if (searchValue.length) {
-      const newData = fuseData.current.search(searchValue).map((i) => i.item)
-      setPaginatedData(newData.slice(0, 10))
+      const newData = fuseData.current
+        .search({ facility_name: searchValue })
+        .filter((i) => (i?.score || 0) <= 0.5)
+        .map((i) => i.item)
+
+      setPaginatedData(newData.slice(skip, end))
     } else {
       setPaginatedData(data.slice(skip, end))
     }
