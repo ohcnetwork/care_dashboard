1159a0455fe7c0bf1e1999bbc4671e89cce538c3
Ritesh-Aggarwal <riteshaggarwal2001@gmail.com>
2022-10-12 22:46:09 +0530

    fix imports

--- src/pages/district/Capacity.tsx
+++ src/pages/district/Capacity.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import useFacilitySummary, {
   FacilitySummaryQuery,
 } from '../../api/queries/useFacilitySummary'
